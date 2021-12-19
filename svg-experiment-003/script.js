8ac5effce2906cb1d95c901cf93f96fd63b415c5
Diego Inácio <diegodci@gmail.com>
2021-12-19 18:58:51 -0300

    Update experiment

--- svg-experiment-003/script.js
+++ svg-experiment-003/script.js
@@ -17,34 +17,101 @@ const MOUSE = {
 
 let CIRCLES = [];
 
+// ! Parameters (from index page)
+const bokeh_radiusMin = document.querySelector("input#bokeh_radiusMin");
+const bokeh_radiusMax = document.querySelector("input#bokeh_radiusMax");
+let RADIUS_MIN_VAR = Math.min(bokeh_radiusMin.value, bokeh_radiusMax.value);
+let RADIUS_MAX_VAR = Math.max(bokeh_radiusMin.value, bokeh_radiusMax.value);
+
+const bokeh_depth = document.querySelector("input#bokeh_depth");
+let DEPTH_VAR = bokeh_depth.value;
+
+const bokeh_density = document.querySelector("input#bokeh_density");
+let DENSITY_VAR = bokeh_density.value;
+
 // ! Parameters
-// * Number of bokehs
-let N = parseInt(Math.sqrt(WIDTH * HEIGHT) / 2);
+// * Bokeh radius
+let radiusMin = (RADIUS_MIN_VAR * Math.min(WIDTH, HEIGHT)) / 256;
+let radiusMax = (RADIUS_MAX_VAR * Math.min(WIDTH, HEIGHT)) / 256;
+let radiusDepth = radiusMax * DEPTH_VAR;
+// * Bokeh density
+let N = DENSITY_VAR * parseInt(Math.sqrt(WIDTH * HEIGHT) / 2);
 // * Maximum parallax offset
 let MAX_OFFSET = Math.max(WIDTH, HEIGHT) / 4;
-// * Circles
-const radiusMin = 5;
-let radiusMax = Math.min(WIDTH, HEIGHT) / 48;
-let radiusDepth = radiusMax * 4;
 
 const isTouch = window.mobileAndTabletCheck();
 
 // ! Utils
 function randomColor() {
-  // * Get random channels
-  let r = Math.random();
-  let g = Math.random();
-  let b = Math.random();
-  // * Get norm
-  let n = Math.sqrt(r * r + g * g + b * b);
-  // * Normalize channels
-  r = Math.floor((255 * r) / n);
-  g = Math.floor((255 * g) / n);
-  b = Math.floor((255 * b) / n);
+  // * Get random hsl channels -> object
+  const h = parseInt(Math.random() * 360);
+  const s = parseInt(50 + Math.random() * 50);
 
   // * Output
-  return `rgb(${r}, ${g}, ${b})`;
+  return `hsl(${h}, ${s}%, 50%)`;
 }
+
+// ! Event functions
+// * Stop propagation when click on input
+function stop_propagation() {
+  event.stopPropagation();
+}
+
+// * Bokeh radius parameter events
+function set_bokeh_radius() {
+  RADIUS_MIN_VAR = Math.min(bokeh_radiusMin.value, bokeh_radiusMax.value);
+  RADIUS_MAX_VAR = Math.max(bokeh_radiusMin.value, bokeh_radiusMax.value);
+  radiusMin = (RADIUS_MIN_VAR * Math.min(WIDTH, HEIGHT)) / 256;
+  radiusMax = (RADIUS_MAX_VAR * Math.min(WIDTH, HEIGHT)) / 256;
+  radiusDepth = radiusMax * DEPTH_VAR;
+  main();
+}
+
+// * Bokeh radius parameter events
+function set_bokeh_depth() {
+  DEPTH_VAR = bokeh_depth.value;
+  radiusDepth = radiusMax * DEPTH_VAR;
+  main();
+}
+
+// * Bokeh density parameter events
+function set_bokeh_density() {
+  DENSITY_VAR = bokeh_density.value;
+  N = DENSITY_VAR * parseInt(Math.sqrt(WIDTH * HEIGHT) / 2);
+  main();
+}
+
+// * Mouse movement
+window.addEventListener(isTouch ? "touchmove" : "mousemove", (event) => {
+  let RECT = DIV.getBoundingClientRect();
+
+  let clientX = isTouch ? event.touches[0].clientX : event.clientX;
+  let clientY = isTouch ? event.touches[0].clientY : event.clientY;
+
+  // * Relative x
+  MOUSE.x = clientX - RECT.left;
+  MOUSE.x /= RECT.right - RECT.left;
+  MOUSE.x = MOUSE.x * WIDTH;
+
+  // * Relative y
+  MOUSE.y = clientY - RECT.top;
+  MOUSE.y /= RECT.bottom - RECT.top;
+  MOUSE.y = MOUSE.y * HEIGHT;
+});
+
+// * Resize window
+window.addEventListener("resize", () => {
+  WIDTH = window.innerWidth;
+  HEIGHT = window.innerHeight;
+
+  radiusMin = (RADIUS_MIN_VAR * Math.min(WIDTH, HEIGHT)) / 256;
+  radiusMax = (RADIUS_MAX_VAR * Math.min(WIDTH, HEIGHT)) / 256;
+  radiusDepth = radiusMax * DEPTH_VAR;
+  N = DENSITY_VAR * parseInt(Math.sqrt(WIDTH * HEIGHT) / 2);
+  MAX_OFFSET = Math.max(WIDTH, HEIGHT) / 4;
+
+  main();
+});
 
 // ! Classes
 class Circle {
@@ -149,33 +216,3 @@ function main() {
 }
 
 main();
-
-// ! Event listeners
-window.addEventListener(isTouch ? "touchmove" : "mousemove", (event) => {
-  let RECT = DIV.getBoundingClientRect();
-
-  let clientX = isTouch ? event.touches[0].clientX : event.clientX;
-  let clientY = isTouch ? event.touches[0].clientY : event.clientY;
-
-  // * Relative x
-  MOUSE.x = clientX - RECT.left;
-  MOUSE.x /= RECT.right - RECT.left;
-  MOUSE.x = MOUSE.x * WIDTH;
-
-  // * Relative y
-  MOUSE.y = clientY - RECT.top;
-  MOUSE.y /= RECT.bottom - RECT.top;
-  MOUSE.y = MOUSE.y * HEIGHT;
-});
-
-window.addEventListener("resize", () => {
-  WIDTH = window.innerWidth;
-  HEIGHT = window.innerHeight;
-
-  N = parseInt(Math.sqrt(WIDTH * HEIGHT) / 2);
-  MAX_OFFSET = Math.max(WIDTH, HEIGHT) / 4;
-  radiusMax = Math.min(WIDTH, HEIGHT) / 48;
-  radiusDepth = radiusMax * 4;
-
-  main();
-});
