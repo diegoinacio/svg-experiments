b6cb6104924fd7e896fe9d8d50beed9106596615
Diego Inácio <diegodci@gmail.com>
2022-06-20 12:22:47 -0300

    Upload metadata

---
+++ metadata.mjs
@@ -0,0 +1,56 @@
+export const INDEX = [
+  // ? Experiment 006 - CellAuto
+  {
+    id: "svg-experiment-006",
+    class: "item",
+    name: "CellAuto",
+    title: "Parameterized one-dimensional cellular automaton.",
+    color: "salmon",
+    stroke: "cyan",
+  },
+  // ? Experiment 005 - PseuDoF
+  {
+    id: "svg-experiment-005",
+    class: "item",
+    name: "PseuDoF",
+    title: "Interactive pseudo Depth of Field effect.",
+    color: "crimson",
+    stroke: "yellow",
+  },
+  // ? Experiment 004 - MPHS DSGN
+  {
+    id: "svg-experiment-004",
+    class: "item",
+    name: "MPHS DSGN",
+    title: "Abstract Memphis Design (80s feelings).",
+    color: "darkcyan",
+    stroke: "lightgreen",
+  },
+  // ? Experiment 003 - Bokeh
+  {
+    id: "svg-experiment-003",
+    class: "item",
+    name: "Bokeh",
+    title: "Interactive Bokeh effect with parallax illusion.",
+    color: "coral",
+    stroke: "white",
+  },
+  // ? Experiment 002 - grā-dē-ənt
+  {
+    id: "svg-experiment-002",
+    class: "item",
+    name: "grā-dē-ənt",
+    title: "Horizontal gradient lines.",
+    color: "darkslategray",
+    stroke: "bisque",
+  },
+  // ? Experiment 001 - ConcentriX
+  {
+    id: "svg-experiment-001",
+    class: "item",
+    name: "ConcentriX",
+    title: "Randomly rotated, inscribed and concentric squares.",
+    color: "black",
+    stroke: "white",
+  },
+];
